e57ac52131c684f35d18b241335d342ff7809cfd
super.sun <121212>
2017-11-17 16:55:25 +0800

    1

---
+++ modules/webapplication/src/main/java/com/dave/sun/filter/MyFilter.java
@@ -0,0 +1,36 @@
+package com.dave.sun.filter;
+
+import com.dave.sun.service.impl.DemoServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * Created by Super.Sun on 2017/11/17.
+ */
+@WebFilter(filterName="myFilter",urlPatterns="/*")
+public class MyFilter implements Filter{
+
+    private static final Logger LOGGER= LoggerFactory.getLogger(MyFilter.class);
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        LOGGER.info("myFilter初始化");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        LOGGER.info("myFilter过滤");
+        String name= request.getParameter("name");
+        LOGGER.info("name="+name);
+        chain.doFilter(request,response);
+
+    }
+
+    @Override
+    public void destroy() {
+        LOGGER.info("myFilter销毁");
+    }
+}
---
+++ modules/webapplication/src/main/java/com/dave/sun/listener/MyHttpSessionListener.java
@@ -0,0 +1,22 @@
+package com.dave.sun.listener;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Created by Super.Sun on 2017/11/17.
+ */
+@WebListener
+public class MyHttpSessionListener implements HttpSessionListener {
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        System.out.println("Session 被创建");
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        System.out.println("ServletContex初始化");
+    }
+}
