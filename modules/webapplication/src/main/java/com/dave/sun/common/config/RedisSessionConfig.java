9e6dc471e9d75aa0f87733a4709d090d796ccbd9
super.sun <121212>
2017-11-22 17:14:22 +0800

    1

---
+++ modules/webapplication/src/main/java/com/dave/sun/common/config/RedisSessionConfig.java
@@ -0,0 +1,11 @@
+package com.dave.sun.common.config;
+
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+//redis-session超时设置
+//@Configuration
+//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400*30)
+public class RedisSessionConfig {
+	
+}
---
+++ modules/webapplication/src/main/java/com/dave/sun/common/config/SessionConfig.java
@@ -0,0 +1,23 @@
+package com.dave.sun.common.config;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Super.Sun on 2017/11/22.
+ */
+@Configuration
+public class SessionConfig {
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer(){
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                container.setSessionTimeout(1800);//单位为S
+            }
+        };
+    }
+}
