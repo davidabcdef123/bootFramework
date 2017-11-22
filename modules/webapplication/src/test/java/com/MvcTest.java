83b12bbe657359f6dd47b7f85cc8190caa5e7af2
super.sun <121212>
2017-11-22 14:53:16 +0800

    1

---
+++ modules/webapplication/src/test/java/com/MvcTest.java
@@ -0,0 +1,51 @@
+package com;
+
+
+import com.dave.sun.Application;
+import com.dave.sun.action.DemoController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by Super.Sun on 2017/11/22.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class MvcTest {
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new DemoController()).build();
+    }
+
+    //验证controller是否正常响应并打印返回结果
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+
+    //验证controller是否正常响应并判断返回结果是否正确
+    @Test
+    public void testHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World")));
+    }
+}
